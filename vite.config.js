a3f6b3d4d70ce2604376b295d13cb675c04563a0
fbenkadhi <fbenkadhi@gmail.com>
2026-04-15 16:01:16 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true,
-  },
   build: {
     outDir: "dist",
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          react:   ["react", "react-dom"],
-          router:  ["react-router-dom"],
-          lucide:  ["lucide-react"],
-        },
-      },
-    },
   },
 });
